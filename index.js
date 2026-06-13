394b8f385afff0b543173df3cd71104ac9cdd0ea
mdujjal67 <mdujjal.mm67@gmail.com>
2026-06-13 17:03:07 +0600

    added mongoose for better database integration

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 const express = require('express');
-const app = express()
-const port = 5000
+const app = express();
+const mongoose = require('mongoose');
+const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://express_backend_user:DXrDbNZUv8TtjFYJ@cluster0.7tyfnet.mongodb.net/?appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+// 1) Schema (blue print), --> 2) Model (Prototype), --> 3) Real Data
+// 1) mongoose schema
+const todoSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  priority: String
 });
+
+// 2) Model with big latter acting like a class
+const Todo = mongoose.model('Todo', todoSchema);  //here 'Todo' inside the functions is for creating collection automatically with lowercase and plural form.
+
+
+const uri = "mongodb+srv://express_backend_user:DXrDbNZUv8TtjFYJ@cluster0.7tyfnet.mongodb.net/todoDB?appName=Cluster0"; // here todoDB after the mongodb.net/ is for creating the database wit name.
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
 
-    const backendCollection = client.db('ExBackendDB').collection('exBackend');
+    await mongoose.connect(uri);
+
 
     app.get('/posts', async (req, res) => {
-        const backend = await backendCollection.find({}).toArray();
-        res.send(backend)
+      // const backend = await backendCollection.find({}).toArray();
+      const todos = await Todo.find({}); //here the Todo is the model Todo
+      res.send(todos)
     });
 
-    app.post('/post', async (req, res)=> {
-        const postdata = req.body;
-        const post =await backendCollection.insertOne(postdata);
-        res.send(post)
+    app.post('/post', async (req, res) => {
+      const todoData = req.body;
+      
+      const todo = new Todo(todoData); //here the Todo after new word is the model todo
+      await todo.save();
+
+      console.log(todo)
+      res.send(todo)
     })
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
+  }
   finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
