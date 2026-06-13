e3ab9041df323090fac986c9c59534e5da1c3fc7
mdujjal67 <mdujjal.mm67@gmail.com>
2026-06-13 18:18:01 +0600

    added data velidation in mongoose

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ app.use(express.json());
 // 1) Schema (blue print), --> 2) Model (Prototype), --> 3) Real Data
 // 1) mongoose schema
 const todoSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+  },
   address: String,
-  priority: String
+  priority: {
+    type: String,
+    enum: ['high', 'medium', 'low'],
+    required: true,
+  }
 });
 
 // 2) Model with big latter acting like a class
@@ -33,9 +40,13 @@ async function run() {
 
     app.post('/post', async (req, res) => {
       const todoData = req.body;
-      
-      const todo = new Todo(todoData); //here the Todo after new word is the model todo
-      await todo.save();
+
+      // option-1
+      const todo = new Todo(todoData); //Step-1: here the Todo after new word is the model todo to create in memory
+      await todo.save(); //Step-2  save to database
+
+      // Or option-2
+      // const todo = await Todo.create(todoData);   --- This alternative does Step 1 and Step 2 at once!---
 
       console.log(todo)
       res.send(todo)
